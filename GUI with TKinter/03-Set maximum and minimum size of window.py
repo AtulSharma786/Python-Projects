304858dc005e41bc64ea89760699a568d94fd48f
Atul Sharma <atulsharma20may@gmail.com>
2020-03-20 12:53:03 +0530

    To upload files

---
+++ GUI with TKinter/03-Set maximum and minimum size of window.py
@@ -0,0 +1,13 @@
+from tkinter import *
+root = Tk()
+
+# width x height
+root.geometry("644x434")
+
+# width, height
+root.minsize(200,100)
+
+# width, height
+root.maxsize(800,400)
+
+root.mainloop()
