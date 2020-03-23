28769c3d53e2e2febbb1898c246e5a98c938bcb8
Atul Sharma <atulsharma20may@gmail.com>
2020-03-23 23:13:47 +0530

    To upload files

---
+++ GUI with TKinter/04-Adding Label.py
@@ -0,0 +1,12 @@
+from tkinter import *
+root = Tk()
+
+# width x height
+root.geometry("600x400")
+
+
+# label
+a = Label(text="atul sharma is a very good boy")
+a.pack() #To pack the label in the root
+
+root.mainloop()
