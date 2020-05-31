1521225f4c5e1c109e8a624de6585077546feb96
Atul Sharma <atulsharma20may@gmail.com>
2020-05-31 17:07:04 +0530

    To upload files

---
+++ 02-Comments.py
@@ -0,0 +1,10 @@
+# This is a single line comment
+
+print("Hello,")  # No need to put \n as it will print automatically in next line
+
+'''This
+    is
+    multiline
+    comment'''
+
+print("My name is Atul Sharma.")
