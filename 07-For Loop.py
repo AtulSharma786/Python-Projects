93a59965cbcb3845793e0e953e5054ec630a0df5
Atul Sharma <atulsharma20may@gmail.com>
2020-06-05 14:05:30 +0530

    To upload files

---
+++ 07-For Loop.py
@@ -0,0 +1,13 @@
+list = [["atul", 1], ["harry", 3], ["rohan", 6]]
+
+dict = dict(list)
+
+for x, y in list:
+    print(x, y)
+
+for x, y in dict.items():
+    print(x, y)
+
+for x in dict:
+    print(x)
+pass
