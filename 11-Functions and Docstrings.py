f0bc758e6863c8bbf9c1ede0385dafa4edc360a1
Atul Sharma <atulsharma20may@gmail.com>
2020-06-06 15:16:06 +0530

    To upload files

---
+++ 11-Functions and Docstrings.py
@@ -0,0 +1,10 @@
+def func1(a,b):
+    """This is a function to calculate average of two numbers"""
+
+    d = a+b
+    return d  # Other wise it show none in output
+
+
+print(func1(5, 6))
+
+print(func1.__doc__)
