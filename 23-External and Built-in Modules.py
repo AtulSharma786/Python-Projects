fb921ce68aa42f79bde9b5e01346d10d40bf725b
Atul Sharma <atulsharma20may@gmail.com>
2020-06-12 09:46:09 +0530

    To upload files

---
+++ 23-External and Built-in Modules.py
@@ -0,0 +1,11 @@
+import random
+
+num = random.randint(0, 5)
+print(num)
+
+rand = random.random()*100
+print(rand)
+
+lst = ["star plus", "DD1", "aaj tak"]
+choice = random.choice(lst)
+print(choice)
---
+++ main_function.py
@@ -0,0 +1,16 @@
+
+
+def printatul(string):
+    return f"ye string {string} ko de de bhai "
+
+
+def add(num1, num2):
+    return num1+num2+5
+
+
+print("and the name is", __name__)
+
+if __name__ == '__main__':
+    print(printatul("atul"))
+    o = add(4, 6)
+    print(o)
