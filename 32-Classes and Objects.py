c077467fb529bf5d4eabcca059bfff0840fedc7c
Atul Sharma <atulsharma20may@gmail.com>
2020-07-08 12:05:22 +0530

    To upload file

---
+++ 32-Classes and Objects.py
@@ -0,0 +1,13 @@
+class Student:
+    pass
+
+
+atul = Student()
+harry = Student()
+
+atul.name = "atul"
+atul.std = 13
+atul.section = 1
+
+harry.subjects = ["hindi", "physics"]
+print(atul.section, harry.subjects)
