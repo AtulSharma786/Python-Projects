47897ef3d931528d5b4868b54f52368b8244c000
Atul Sharma <atulsharma20may@gmail.com>
2020-07-17 15:45:36 +0530

    To upload file

---
+++ 52-Else with For loop.py
@@ -0,0 +1,10 @@
+khana = ["roti", "sabzi", "chawal"]
+
+for item in khana:
+    print(item)
+    # break
+
+# else use with for loop when loop ended normally means without break and error
+
+else:
+    print("This for loop ended properly")
